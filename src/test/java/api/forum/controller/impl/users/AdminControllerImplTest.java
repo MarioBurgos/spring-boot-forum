a8561bf24d18f4cd0740314910672512937fa4c8
Mario Burgos <mario.burpi@gmail.com>
2023-03-08 17:06:44 +0100

    Tested findAll() method

---
+++ src/test/java/api/forum/controller/impl/users/AdminControllerImplTest.java
@@ -0,0 +1,136 @@
+package api.forum.controller.impl.users;
+
+import api.forum.model.enums.Shift;
+import api.forum.model.enums.Status;
+import api.forum.model.users.Admin;
+import api.forum.repository.users.AdminRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdminControllerImplTest {
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private MockMvc mockMvc;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private Admin admin1, admin2;
+    private MvcResult mvcResult;
+
+    @BeforeEach
+    void setUp() {
+        admin1 = new Admin("IMANADMIN", "EMAIL", "PASSWORD");
+        admin1.setShift(Shift.MORNING);
+        admin1.setLocation("UK");
+        admin1.setStatus(Status.DISCONNECTED);
+        admin1.setLastLogIn(Date.valueOf(LocalDate.now()));
+        admin2 = new Admin("anotherone", "email@email", "PASSWORD");
+        admin2.setShift(Shift.NIGHT);
+        admin2.setLocation("ITA");
+        admin2.setStatus(Status.ON_VACATION);
+        admin2.setLastLogIn(Date.valueOf(LocalDate.of(2023, 1, 1)));
+        adminRepository.saveAll(List.of(admin1, admin2));
+    }
+
+    @AfterEach
+    void tearDown() {
+        adminRepository.deleteAll();
+    }
+
+    @Test
+    void findAll() throws Exception {
+        mvcResult = mockMvc.perform(get("/admins"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        JSONArray jsonArray = new JSONArray(mvcResult.getResponse().getContentAsString());
+        assertTrue(jsonArray.get(0).toString().contains(admin1.getUserName()));
+        assertTrue(jsonArray.get(1).toString().contains(admin2.getLocation()));
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByUserName() {
+    }
+
+    @Test
+    void findByEmail() {
+    }
+
+    @Test
+    void findByLastLogInGreaterThan() {
+    }
+
+    @Test
+    void findByLastLogInBetween() {
+    }
+
+    @Test
+    void findByStatus() {
+    }
+
+    @Test
+    void findByShift() {
+    }
+
+    @Test
+    void findByLocation() {
+    }
+
+    @Test
+    void addNewAdmin() {
+    }
+
+    @Test
+    void updateAdmin() {
+    }
+
+    @Test
+    void updateAdminEmail() {
+    }
+
+    @Test
+    void updateAdminPassword() {
+    }
+
+    @Test
+    void updateLastLogIn() {
+    }
+
+    @Test
+    void updateStatus() {
+    }
+
+    @Test
+    void updateShift() {
+    }
+
+    @Test
+    void updateLocation() {
+    }
+}
